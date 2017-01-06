4cef034fe8792b4fa5891db37fa39dc7caeba0d3
Jordan Conard <jconard@RSG-2585-Jordan-Conard.local>
2017-01-06 13:24:18 -0500

    Added three new lookup functions and a delete droplet function.

--- do.go
+++ do.go
@@ -2,6 +2,7 @@ package main
 
 import "os"
 import "fmt"
+import "errors"
 
 import "golang.org/x/oauth2"
 import "github.com/digitalocean/godo"
@@ -43,6 +44,38 @@ func ListDroplets(client godo.Client) {
 	for _, element := range droplets {
 		fmt.Println(element.Name)
 	}
+}
+
+func DeleteDroplet(client godo.Client, id int) error {
+	_, err := client.Droplets.Delete(id)
+	return err
+}
+
+func RetrieveDroplet(client godo.Client, id int) (*godo.Droplet, error) {
+	droplet, _, err := client.Droplets.Get(id)
+	return droplet, err
+}
+
+func IDLookupDroplet(client godo.Client, name string) (int, error) {
+	opt := &godo.ListOptions{
+		Page:    1,
+		PerPage: 25,
+	}
+	droplets, _, _ := client.Droplets.List(opt)
+	for _, element := range droplets {
+		if element.Name == name {
+			return element.ID, nil
+		}
+	}
+	return 0, errors.New("No droplet matched the given name.\n")
+}
+
+func NameLookupDroplet(client godo.Client, id int) (string, error) {
+	droplet, err := RetrieveDroplet(client, id)
+	if err == nil {
+		return droplet.Name, nil
+	}
+	return "", errors.New("No droplet matched the given ID.\n")
 }
 
 func main() {
