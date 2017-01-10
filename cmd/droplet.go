5eb9539ed6cb7f9c28fbb36b39549517daa693fc
Jordan Conard <jconard@RSG-2585-Jordan-Conard.local>
2017-01-10 15:44:50 -0500

    Added short and long descriptions of droplet subcommand.

--- cmd/droplet.go
+++ cmd/droplet.go
@@ -23,13 +23,9 @@ import (
 // dropletCmd represents the droplet command
 var dropletCmd = &cobra.Command{
 	Use:   "droplet",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Interface for DigitalOcean Droplets",
+	Long: `Droplet subcommand provides droplet-level interface with the
+	provided DigitalOcean account.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
 		fmt.Println("droplet called")
